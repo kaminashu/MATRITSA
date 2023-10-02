0ad62d52bfaa9375b816a500b8f2ed8de92a3bbe
kaminashu <ertertfd89werwe@mail.ru>
2023-10-02 20:47:22 +0500

    Initial commit

---
+++ app/src/main/java/www/uzmd/matritsa/presentation/MainActivity.kt
@@ -0,0 +1,16 @@
+package www.uzmd.matritsa.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import www.uzmd.matritsa.R
+import www.uzmd.matritsa.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportFragmentManager.beginTransaction().replace(R.id.conatainer, TanishuvFragment()).commit()
+    }
+}
